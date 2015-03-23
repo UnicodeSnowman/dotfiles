9415eb0e8c365fbc432d2abbd3cb9b290404a8ec
unicodesnowman <utfsnowman@gmail.com>
2015-03-23 11:55:23 -0400

    use var

--- setup.sh
+++ setup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 for file in $( ls $(dirname $0)/home ); do
+
+    FILE_PATH=$(dirname $0)/home/$file
+
     if [ ! -L ~/.$file ]; then
         echo "~/.$file not here, symlinking"
-        ln -s $(dirname $0)/home/$file ~/.$file
+        ln -s $FILE_PATH ~/.$file
     else
         echo "$file already exists, deleting and re-linking"
 	rm ~/.$file
-        ln -s $(dirname $0)/home/$file ~/.$file
+        ln -s $FILE_PATH ~/.$file
     fi
 done
